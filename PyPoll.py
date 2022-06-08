13181e21cefc557fd9a0b6c69725ea04b84ec904
John Hendricks <jwh8zc@virginia.edu>
2022-06-08 16:41:19 -0400

    Added the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -6,10 +6,56 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#initiate accumulator
+total_votes = 0
+
+# list the name of candidates
+candidate_options = []
+
+# total votes for each candidate
+candidate_votes = dict()
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    # Read and print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
+        total_votes += 1
+        candidate_name = row[2]
+        if (candidate_name not in candidate_options):
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+
+for candidate_name in candidate_votes:
+    votes = candidate_votes[candidate_name]
+    vote_percentage = float(votes) / float(total_votes) * 100
+    print(f"{candidate_name} received {vote_percentage}% of the vote.")
+
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+    
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+
+# print(total_votes)
+# print the candidate list
+print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
